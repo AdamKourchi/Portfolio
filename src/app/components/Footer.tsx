8c96a2f2a4f2b69d67c9b495463c0a332bade371
AdamKourchi <9orachiadam@gmail.com>
2025-10-20 11:41:24 +0100

    Add CV + Fixed Theme bug

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,19 +1,40 @@
 import Image from "next/image";
+import { useTheme } from "next-themes";
+import { useEffect } from "react";
+
+
 
 export default function Footer() {
   const year = new Date().getFullYear();
+    const { theme } = useTheme();
+
+  
   return (
     <footer className="w-full flex flex-col items-center justify-center gap-5 my-5">
       <p className="text-center dark:text-white text-black">
         © {year} Adam Kourchi. All rights reserved.
       </p>
-      
+
+      {
+        theme == "dark" && 
       <Image
-        src="/images/logo-white.png"
+        src="/images/logo-dark.png"
         alt="My Logo"
         width={70}
         height={70}
       />
+      }
+
+           {
+        theme == "light" && 
+      <Image
+        src="/images/logo-light.png"
+        alt="My Logo"
+        width={70}
+        height={70}
+      />
+      }
+      
     </footer>
   );
 }
--- src/app/components/VantaRings.jsx
+++ src/app/components/VantaRings.jsx
@@ -10,39 +10,40 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useTheme } from "next-themes";
 
 export default function VantaRings() {
   const vantaRef = useRef(null);
   const [vantaEffect, setVantaEffect] = useState(null);
+  const { theme } = useTheme();
 
   useEffect(() => {
-    if (!vantaEffect) {
-      setVantaEffect(
-        RINGS({
-          el: vantaRef.current,
-          THREE,
-          mouseControls: true,
-          touchControls: true,
-          gyroControls: false,
-          minHeight: 200.0,
-          minWidth: 200.0,
-          scale: 1.0,
-          scaleMobile: 1.0,
-          backgroundColor: 0x202428,
-          color: 0xff3f81,
-          backgroundAlpha: 1.0,
-        })
-      );
-    }
+
+    if (vantaEffect) vantaEffect.destroy(); 
+    const isDark = theme === "dark";
+
+    const effect = RINGS({
+      el: vantaRef.current,
+      THREE,
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      scale: 1.0,
+      scaleMobile: 1.0,
+      backgroundColor:  0x202428,
+      color: 0xff3f81,
+      backgroundAlpha: isDark ? 1.0 : 4.0, 
+    });
+
+    setVantaEffect(effect);
 
     return () => {
-      if (vantaEffect) vantaEffect.destroy();
-    };
-  }, [vantaEffect]);
+      effect.destroy();
+    };  }, [theme]);
 
   return (
     <div ref={vantaRef} className="w-full h-[500px]">
@@ -58,28 +59,32 @@ export default function VantaRings() {
       </div>
 
       <div className="flex-col justify-start text-white h-5/6  items-start ml-10 lg:ml-32 mt-20 gap-6">
-        <h1 className="text-4xl lg:text-6xl font-semibold z-10">Adam Kourchi</h1>
-        <p className="text-lg lg:text-xl mt-5 z-10">A Full-Stack Web Developer.</p>
+        <h1 className="text-4xl lg:text-6xl font-semibold z-10">
+          Adam Kourchi
+        </h1>
+        <p className="text-lg lg:text-xl mt-5 z-10">
+          A Full-Stack Web Developer.
+        </p>
 
         <DropdownMenu modal={false}>
           <DropdownMenu>
-            <DropdownMenuTrigger className="mt-5 cursor-none"  asChild>
-              <Button variant="outline">
+            <DropdownMenuTrigger className="mt-5 cursor-none" asChild>
+              <Button className="dark:text-white text-black" variant="outline">
                 <Download />
                 CV
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
-              <DropdownMenuItem className="cursor-none">
-                <a className="cursor-none" href="cv-english.pdf" download>
+              <a className="cursor-none" href="cv-english.pdf" download>
+                <DropdownMenuItem className="cursor-none">
                   English
-                </a>
-              </DropdownMenuItem>
-              <DropdownMenuItem className="cursor-none"> 
-                <a className="cursor-none" href="cv-french.pdf" download>
+                </DropdownMenuItem>
+              </a>
+              <a className="cursor-none" href="cv-french.pdf" download>
+                <DropdownMenuItem className="cursor-none">
                   Français
-                </a>
-              </DropdownMenuItem>
+                </DropdownMenuItem>
+              </a>
             </DropdownMenuContent>
           </DropdownMenu>
         </DropdownMenu>
